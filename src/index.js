eb6e5dd6399eb16168253bda661affcf13186fc0
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-01 16:58:11 -0700

    End of lesson 26. Decode JWT, extract user id, me query, resolver.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const cookieParser = require('cookie-parser');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -7,7 +7,17 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO: Use express middleware to populate current user
+
+// Decode the JWT to get the user on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if(token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // Put the userId on the req
+    req.userId = userId;
+  }
+  next();
+})
 
 server.start({
   cors: {
