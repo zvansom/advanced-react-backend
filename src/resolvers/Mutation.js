48767bf094d3a5e338bf45989a43717cb7e28b1f
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-04 11:15:56 -0800

    End of lesson 28. Signout mutation clears cookie.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -83,6 +83,10 @@ const Mutations = {
     // Return the user
     return user;
   },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
+  }
 };
 
 module.exports = Mutations;
