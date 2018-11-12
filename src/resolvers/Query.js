5703a6b71d56e43001bd4b97fa9a0206e362e38b
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-12 15:50:33 -0800

    End of lesson 53. Orders query and resolver created

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -45,6 +45,17 @@ const Query = {
     // return the order
     return order;
   },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if(!userId) {
+      throw new Error('You must be logged in');
+    }
+    return ctx.db.query.orders({
+      where: {
+        user: { id: userId },
+      },
+    }, info);
+  },
 };
 
 module.exports = Query;
