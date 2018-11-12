89e3c28090a1930558f922a132c727bce67319f7
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-12 15:14:26 -0800

    End of lesson 53. Created order query. Permission based.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -26,7 +26,25 @@ const Query = {
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
     // Query the users
     return ctx.db.query.users({}, info);
-  }
+  },
+  async order(parent, args, ctx, info) {
+    // Make sure user is logged in
+    if(!ctx.request.userId) {
+      throw new Error(`You aren't logged in!`);
+    }
+    // Query the current order
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+    }, info);
+    // Check if they have permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if(!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error(`You don't have permission to view this.`)
+    }
+    // return the order
+    return order;
+  },
 };
 
 module.exports = Query;
