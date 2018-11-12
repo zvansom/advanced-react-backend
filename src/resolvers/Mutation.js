110e904f67eb192215fcfb135c274ae4713e59e6
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-12 14:42:00 -0800

    End of lesson 51. Orders save to db, user cart emptied.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -279,6 +279,7 @@ const Mutations = {
           id 
           description 
           image
+          largeImage
         }
       }
     }
@@ -293,9 +294,33 @@ const Mutations = {
       source: args.token,
     });
     // Convert the CartItems to OrderItems
+    const orderItems = user.cart.map(cartItem => {
+      const orderItem = {
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: { connect: { id: userId } },
+      };
+      delete orderItem.id;
+      return orderItem;
+    });
     // Create the Order
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } },
+      },
+    });
     // Clear the user's cart, delete cartItems
+    const cartItemIds = user.cart.map(cartItem => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({ 
+      where: {
+        id_in: cartItemIds,
+      }
+    });
     // Return the Order to the client
+    return order;
   }
 };
 
